c308223b3a677a4a80ec0dda6b1ccc76f8918b09
Raphael Oliveira Xavier <raphael@mbp-de-raphael.lan>
2020-06-16 01:21:04 -0300

    finalmente

---
+++ agora vai/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  agora vai
+//
+//  Created by Raphael Oliveira Xavier on 16/06/20.
+//  Copyright © 2020 Raphael Oliveira Xavier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ agora vaiTests/agora_vaiTests.swift
@@ -0,0 +1,34 @@
+//
+//  agora_vaiTests.swift
+//  agora vaiTests
+//
+//  Created by Raphael Oliveira Xavier on 16/06/20.
+//  Copyright © 2020 Raphael Oliveira Xavier. All rights reserved.
+//
+
+import XCTest
+@testable import agora_vai
+
+class agora_vaiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
